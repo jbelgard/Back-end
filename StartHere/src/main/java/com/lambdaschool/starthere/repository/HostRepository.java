8c3bd5ff8645e694e99977a1c7775e048978af81
Jason Belgard <belgard.jason@gmail.com>
2019-08-01 08:52:23 -0500

    bug fix

--- StartHere/src/main/java/com/lambdaschool/starthere/repository/HostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lambdaschool.starthere.repository;
-
-import com.lambdaschool.starthere.models.Host;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HostRepository extends CrudRepository<Host, Long>{
-
-}
